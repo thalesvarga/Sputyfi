dd4daee62db13a603475300c2ea5337bdb921db1
thalesvarga <thalesfotos@hotmail.com>
2025-02-19 13:15:10 -0300

    Removendo back-end do controle de versão

--- back-end/API/connect.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient} from "mongodb";
-
-const URI ="mongodb+srv://thalesvarga:jurubatuba@cluster0.sr0lo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-const client = new MongoClient(URI)
-
-export const db = client.db("Sputyfi")
-//  const songCollection = await db.collection('songs').find({}).toArray();
