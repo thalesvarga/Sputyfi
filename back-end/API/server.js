1a05fe1cf8b8bdbf0dfd254523111553b0e70914
thalesvarga <thalesfotos@hotmail.com>
2025-02-19 11:52:04 -0300

    Ajustes para o deploy

--- back-end/API/server.js
+++ back-end/API/server.js
@@ -1,18 +1,27 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
 
+const __dirname = path.resolve()
+console.log(__dirname);
 const app = express();
 const PORT = 3000;
 
 app.use(cors())
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, '../front-end/dist')))
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, '../front-end/dist/index.html'));
 });
 
 
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,8 @@
+import "dotenv/config";
 import axios from "axios";
 
-const URL = "http://localhost:3000"
+const {NODE_ENV} = process.env
+const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api";
 
 const responseArtists = await axios.get(`${URL}/artists`)
 const responseSongs = await axios.get(`${URL}/songs`)
