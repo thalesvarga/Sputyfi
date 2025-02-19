070a55368e4b84a13da18ed24cb27d85884b4a30
thalesvarga <thalesfotos@hotmail.com>
2025-02-19 13:15:42 -0300

    Reenviando back-end com nome corrigido

---
+++ back-end/api/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve()
+console.log(__dirname);
+const app = express();
+const PORT = 3000;
+
+app.use(cors())
+
+app.get("/api/songs", async (request, response) => {
+  response.send(await db.collection("songs").find({}).toArray());
+});
+
+app.get("/api/artists", async (request, response) => {
+  response.send(await db.collection("artists").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, '../front-end/dist')))
+
+app.get("*", async (request, response) => {
+  response.sendFile(path.join(__dirname, '../front-end/dist/index.html'));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Servidor esta me escutando na porta ${PORT}`);
+});
