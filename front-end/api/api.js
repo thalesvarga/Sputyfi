3097fff601a3ed60074257219c08ba40bdbc43fc
thalesvarga <thalesfotos@hotmail.com>
2025-02-19 13:29:37 -0300

    Ajustes no process env

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,7 +1,5 @@
-import "dotenv/config";
 import axios from "axios";
 
-// const {NODE_ENV} = process.env
 const URL =  "https://sputyfi.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`)
